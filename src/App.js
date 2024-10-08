c9b4155b5abdd73378df7e1927129f4abe2ac519
carrizobrebeca <carrizob.rebeca@gmail.com>
2024-10-08 13:53:53 -0300

    modificando ruta

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/DejaVuMarke" element={<Home />} />
         <Route path="/info" element={<Info />} />
        
       </Routes>
